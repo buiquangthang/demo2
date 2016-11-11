e40d4b2d5b4117dd160ea73bb8ad94ed8161ba8e
buiquangthang <buiquangthangit@gmail.com>
2016-11-11 11:56:13 +0700

    send email activation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,6 +71,22 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  # Send mail
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default :charset => "utf-8"
+
+  config.action_mailer.smtp_settings = {
+    user_name:      ENV["GMAIL_SMTP_USER"],
+    password:       ENV["GMAIL_SMTP_PASSWORD"],
+    domain:         'gmail.com',
+    address:       'smtp.gmail.com',
+    port:          '587',
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
+
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
